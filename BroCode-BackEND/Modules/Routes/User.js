b6ebab149052684e532a8325366fa4a12ab8cf4e
AmineTouihri <mohamedaminetouihri9@gmail.com>
2022-04-21 14:28:40 +0100

    backend auth

---
+++ BroCode-BackEND/Modules/Routes/User.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const route=express.Router();
+const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken");
+const User=require("../Models/User");
+
+//-------------------------SIGNuP--------------------------------
+route.post("/signUp",(req,res,next)=>{
+bcrypt.hash(req.body.password,10).then(hash=>{
+    const user=new User({
+        email:req.body.email,
+        password:hash
+    });
+    user.save().then(result=>{
+        res.status(201).json({message:"user added succesfuly",result:result})
+    }).catch(error=>{res.status(400).json({message:"oups we can't add user somthing went wrong!"})})
+})
+})
+//---------------------------Login---------------------------------
+route.post("/login",(req,res,next)=>{
+let fetchedUser;
+User.findOne({email:req.body.email}).then(user=>{
+    console.log(user);
+    fetchedUser=user;
+    if (!user){
+        res.status(404).json({message:"user not found"});
+    }
+    return bcrypt.compare(req.body.password,fetchedUser.password)
+}).then(result=>{
+    if(!result){
+        res.status(404).json({message:"faild to connect!"})
+    }
+    const token=jwt.sign({email:fetchedUser.email,userId:fetchedUser._id},
+                          "secret_this_should_be_longer",
+                             {expiresIn: "1h"}
+        );
+    res.status(201).json({message:"connected !",token:token});
+}).catch(error=>{
+    console.log(error);
+    res.status(400).json({message:"somthing went wrong!"})
+})
+})
+module.exports=route;
