56d6975c2c08641e5a935b9e3247e4640e11b74e
Hazem Ben Saria <bensariahazem@gmail.com>
2022-04-21 14:45:23 +0100

    i updated the landing page

--- BroCode-FrontEND/src/app/app-routing.module.ts
+++ BroCode-FrontEND/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./Components/Public/landing-page/landing-page.component";
+import {LoginComponent} from "./Components/Public/login/login.component";
+import {RegisterComponent} from "./Components/Public/register/register.component";
 
 
 const routes: Routes = [
+  {path : "" ,  component : LandingPageComponent},
+  {path : "login" ,  component : LoginComponent},
+  {path : "register" ,  component : RegisterComponent},
 
 ];
 
