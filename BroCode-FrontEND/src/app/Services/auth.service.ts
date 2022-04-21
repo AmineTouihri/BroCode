32c1b1b98299f3c6a411579ff679cf56f06ded13
AmineTouihri <mohamedaminetouihri9@gmail.com>
2022-04-21 18:00:51 +0100

    add interactor

---
+++ BroCode-FrontEND/src/app/Services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {authModel} from "../Models/auth-Model";
+import {HttpClient} from "@angular/common/http";
+import {Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private authListner=new Subject<boolean>();//bch na3rfou bih est ce que fama user mconecti bch ndesplayi 7ajet fel header w fazet
+private token!:string;
+public  getToken(){
+  return this.token;
+}
+public getAuthListner(){
+  return this.authListner;
+}
+  constructor(private http:HttpClient) { }
+
+  //------------------------login---------------------------------
+  loginUser(email:string,password:string){
+const auth:authModel={email:email,password:password}
+    this.http.post<{token:string}>('http://localhost:8000/api/user/login',auth).subscribe(result=>{
+      this.token=result.token;
+      this.authListner.next(true);
+    })
+  }
+  //----------------------------register---------------------------------
+
+  createUser(email:string,password:string){
+  const user:authModel={email:email,password:password}
+    this.http.post('http://localhost:8000/api/user/signup',user).subscribe(result=>{
+      console.log("result");
+    })
+  }
+
+
+}
