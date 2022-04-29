5a3d78517697ca11bfa802473b901dcc282e5d40
AmineTouihri <mohamedaminetouihri9@gmail.com>
2022-04-29 22:29:26 +0100

    server set errerus

--- BroCode-BackEND/app.js
+++ BroCode-BackEND/app.js
@@ -8,17 +8,18 @@ const  Connection  = require('./config/DBConnection.js');
 Connection();
 
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 //----------------------------setHeaders------------------------------------------------
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Headers","Origin , X-Requested-With , Content-Type , Accept,authorization");//authorization to store the token
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin , X-Requested-With , Content-Type , Accept,authorization");//authorization to store the token
     res.header("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,DELETE,OPTIONS");
     next();
 })
 
 //-----------------------------userRoute----------------
-app.use("api/user",userRoutes);
+app.use("/api/user",userRoutes); //i made a mistake here i forgot the / in "api/user"
 app.get("/",()=>{
     console.log("helo")})
 
